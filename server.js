c69ed4b42bccda20cabd522a516d6aef7203a108
Tanya Milburn <tanyamilburn89@gmail.com>
2022-08-10 16:03:41 -0600

    removing node modules

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
-// const sequelize = require('./config/connection');
+const sequelize = require('./config/connection');
 
 const app = express()
 const PORT = process.env.PORT || 3001;
